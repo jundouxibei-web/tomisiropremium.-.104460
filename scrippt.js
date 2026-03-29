b11d6565983898c591fddd72a970fa2e698e7355
jundouxibei-web <jundouxibei@gmail.com>
2026-03-29 21:31:00 +0900

    scrippt.js

--- scrippt.js
+++ scrippt.js
@@ -1,6 +1,6 @@
 'use strict';
 console.log('yaa');
-
+document.getElementById("moji").textContent=("")
 function goNext() {
     const btn = document.querySelector('.btn');
 btn.classList.add('move-out');
