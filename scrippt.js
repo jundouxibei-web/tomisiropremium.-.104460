395d3d68f3f9badcb5e1a2d0ce3f9878ef6a2b38
jundouxibei-web <jundouxibei@gmail.com>
2026-03-30 22:25:06 +0900

    scrippt.js

--- scrippt.js
+++ scrippt.js
@@ -1,10 +1,42 @@
 'use strict';
 console.log('yaa');
-document.getElementById("moji").textContent=("")
-function goNext() {
+function goNext0() {
     const btn = document.querySelector('.btn');
 btn.classList.add('move-out');
   setTimeout(() => {
     location.href = 'index2.html';
   }, 600);
 }
+
+
+function check1() {
+    
+  document.querySelector('.toi1').style.display = 'none';
+  document.querySelector('.kai1').style.display = 'block';
+}
+
+function goNext1() {
+    const btn = document.querySelector('.btn11');
+btn.classList.add('move-out');
+  setTimeout(() => {
+    location.href = 'index3.html';
+  }, 100);
+}
+
+function check2() {
+    
+  document.querySelector('.toi2').style.display = 'none';
+  document.querySelector('.kai2').style.display = 'block';
+}
+
+function goNext2() {
+    const btn = document.querySelector('.btn22');
+btn.classList.add('move-out');
+  setTimeout(() => {
+    location.href = 'index4.html';
+  }, 100);
+}
+
+
+
+
